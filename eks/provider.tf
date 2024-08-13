26fd22cdcc66f51e728ceafc743d1252a0b17cdd
sviswanath1419 <s.viswanath1419@outlook.com>
2024-08-13 23:14:36 +0530

    aws s3 bucket name given which is already existed

--- eks/provider.tf
+++ eks/provider.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "my-eks-buket-viswa"
+    bucket         = "terraform-state-bucket-tf-viswa"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
